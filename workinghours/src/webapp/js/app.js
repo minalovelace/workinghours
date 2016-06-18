f22f193829ca7a4243b9218fd029df64fae765fa
Nina Schicketanz <Nina.Schicketanz@gmx.de>
2016-06-18 16:57:58 +0200

    cleanup

--- workinghours/src/webapp/js/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// Declare app level module which depends on filters, and services
-angular.module('RestServer', ['filters', 'services', 'directives', 'controllers']).
-    config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-        $routeProvider.otherwise({redirectTo: '/view1'});
-    }]);
