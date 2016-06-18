1ecb2277303249d85e82c31e5d124a407444d8b0
Nina Schicketanz <Nina.Schicketanz@gmx.de>
2016-06-18 18:58:52 +0200

    Interim commit

--- workinghours/src/RestServer/ResourceHandler.java
+++ workinghours/src/RestServer/ResourceHandler.java
@@ -6,6 +6,9 @@ import java.nio.file.Paths;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 @Path("/")
 public class ResourceHandler
@@ -15,11 +18,37 @@ public class ResourceHandler
     @GET
     public String index() throws IOException
     {
-        return new String(fileLoader(Paths.get(RELATIVE_BASE_PATH + "index.html")), "UTF-8");
+        return new String(fileLoader("index.html"), "UTF-8");
     }
 
-    private byte[] fileLoader(java.nio.file.Path path) throws IOException
+    @GET
+    @Path("css/{fileName}")
+    public String cssFiles(@PathParam("fileName") String fileName) throws IOException
     {
+        return new String(fileLoader("css/" + fileName), "UTF-8");
+    }
+
+    @GET
+    @Produces("text/javascript")
+    @Path("lib/angular/{fileName}")
+    public Response angularFiles(@PathParam("fileName") String fileName) throws IOException
+    {
+        String responseParam = new String(fileLoader("lib/angular/" + fileName), "UTF-8");
+        return Response.ok(responseParam).build();
+    }
+
+    @GET
+    @Produces("text/javascript")
+    @Path("app/{fileName}")
+    public Response appFiles(@PathParam("fileName") String fileName) throws IOException
+    {
+        String responseParam = new String(fileLoader("lib/angular/" + fileName), "UTF-8");
+        return Response.ok(responseParam).build();
+    }
+
+    private byte[] fileLoader(String relPath) throws IOException
+    {
+        java.nio.file.Path path = Paths.get(RELATIVE_BASE_PATH + relPath);
         return Files.readAllBytes(path);
     }
 }
