88b9d0b2f5199a2fe6a1fcd33c6d4754c0d0acd8
Nina Schicketanz <Nina.Schicketanz@gmx.de>
2016-06-23 22:21:50 +0200

    Finish tutorial with in-memory-rest-service

--- workinghours/webapp/app/app.component.ts
+++ workinghours/webapp/app/app.component.ts
@@ -1,11 +1,9 @@
-import { Component }       from '@angular/core';
+import { Component } from '@angular/core';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
-
-import { HeroService }     from './hero.service';
-import { HeroesComponent } from './heroes.component';
-import { DashboardComponent } from './dashboard.component';
+import { DashboardComponent }  from './dashboard.component';
+import { HeroesComponent }     from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
-
+import { HeroService }         from './hero.service';
 @Component({
   selector: 'my-app',
   template: `
@@ -16,33 +14,18 @@ import { HeroDetailComponent } from './hero-detail.component';
     </nav>
     <router-outlet></router-outlet>
   `,
+  styleUrls: ['app/app.component.css'],
   directives: [ROUTER_DIRECTIVES],
   providers: [
     ROUTER_PROVIDERS,
-    HeroService
-  ],
-  styleUrls: ['app/app.component.css']
+    HeroService,
+  ]
 })
-
 @RouteConfig([
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: DashboardComponent,
-    useAsDefault: true
-  },
-  {
-    path: '/detail/:id',
-    name: 'HeroDetail',
-    component: HeroDetailComponent
-  },
-  {
-    path: '/heroes',
-    name: 'Heroes',
-    component: HeroesComponent
-  }
+  { path: '/dashboard',  name: 'Dashboard',  component: DashboardComponent, useAsDefault: true },
+  { path: '/detail/:id', name: 'HeroDetail', component: HeroDetailComponent },
+  { path: '/heroes',     name: 'Heroes',     component: HeroesComponent }
 ])
-
 export class AppComponent {
   title = 'Tour of Heroes';
 }
