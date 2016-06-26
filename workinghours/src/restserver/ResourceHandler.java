b17ca3a6bae26bbaa3f75e65095e520e944bc989
Nina Schicketanz <Nina.Schicketanz@gmx.de>
2016-06-26 17:55:54 +0200

    Make ResourceHandler more generic again

--- workinghours/src/restserver/ResourceHandler.java
+++ workinghours/src/restserver/ResourceHandler.java
@@ -23,35 +23,12 @@ public class ResourceHandler
     }
 
     @GET
-    @Path("index.html")
-    @Produces("text/html")
-    public String rootResourceIndex() throws IOException
+    @Path("{filename}")
+    @Produces(
+    { "text/html", "text/css", "application/json" })
+    public String rootResourceFiles(@PathParam("filename") String filename) throws IOException
     {
-        return new String(fileLoader("index.html"), "UTF-8");
-    }
-
-    @GET
-    @Path("styles.css")
-    @Produces("text/css")
-    public String rootResourceStyles() throws IOException
-    {
-        return new String(fileLoader("styles.css"), "UTF-8");
-    }
-
-    @GET
-    @Path("favicon.ico")
-    @Produces("media/image")
-    public String rootResourceFavIcon() throws IOException
-    {
-        return new String(fileLoader("favicon.ico"), "UTF-8");
-    }
-
-    @GET
-    @Path("systemjs.config.js")
-    @Produces("text/javascript")
-    public String rootResourceSystemConfigJs() throws IOException
-    {
-        return new String(fileLoader("systemjs.config.js"), "UTF-8");
+        return new String(fileLoader(filename), "UTF-8");
     }
 
     @GET
